d7fab5984c41b63bec0b099e4993d67af9b8b889
MorenoMarkovic <35739460+MorenoMarkovic@users.noreply.github.com>
2018-04-10 12:21:13 +0200

    Add files via upload

---
+++ MarkovicMarinoKnjiga/MarkovicMarinoKnjiga/K njigaControl1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MarkovicMarinoKnjiga
+{
+    class K_njigaControl1
+    {
+        List<Poglavlje> PopisPoglavlje = new List<Poglavlje>();
+    }
+}
---
+++ MarkovicMarinoKnjiga/MarkovicMarinoKnjiga/Poglavlje.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovicMarinoKnjiga
+{
+    class Poglavlje
+    {
+        private string NazivPoglavlja;
+        private int brojStranice, ukupanBrojStranica;
+        public void setNazvPoglavlja(string imePoglavlja)
+        {
+            NazivPoglavlja = imePoglavlja;
+        }
+        public void setBrojStranice(int brojStranice2)
+        {
+            brojStranice = brojStranice2;
+        }
+        public void setUkupanBrojStranica(int zbrojStranica)
+        {
+            ukupanBrojStranica = zbrojStranica;
+        }
+        public string getNazivPoglavlja()
+        {
+            return NazivPoglavlja;
+        }
+        public int getBrojStranice()
+        {
+            return brojStranice;
+        }
+        public int getUkupanBrojStranica()
+        {
+            return ukupanBrojStranica;
+        }
+
+    }
+}
